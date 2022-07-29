bd221f35202384b85a4e85ece5e373822b1039f5
Tatiana <tatianabohrer@gmail.com>
2022-07-29 17:58:07 -0400

    change PLAN.md file

---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import Account from './Account';
+import Twoot from './Twoot';
+import TwootList from './TwootList';
+import axios from 'axios';
+
+export default function Main() {
+
+}
---
+++ frontend/src/components/TwootList.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import axios from "axios";
+
+export default function TwootList(props) {
+    
+}
