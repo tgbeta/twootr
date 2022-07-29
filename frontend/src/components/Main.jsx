50bad58519c927de01c923b3273c099f8212022c
Nicolas Meza <nickismeza@gmail.com>
2022-07-29 15:19:22 -0700

    changing main js

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Account from './Account';
 import Twoot from './Twoot';
 import TwootList from './TwootList';
-import axios from 'axios';
 
 export default function Main() {
 
 
     return (
-        <main>
-            <Account />
+        <>
             <Twoot />
             <TwootList />
-        </main>
+        </>
     );
 }
