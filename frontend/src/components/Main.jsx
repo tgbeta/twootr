a5dada17e6e7e5cfe1900de11db79b077710bde9
Nicolas Meza <nickismeza@gmail.com>
2022-07-29 22:30:58 -0700

    change on main

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Twoot from './Twoot';
 import TwootList from './TwootList';
 import AvatarSection from './AvatarSection';
 import Account from './Account';
+
 export default function Main() {
 
     const [twoot, setTwoot] = useState([]);
