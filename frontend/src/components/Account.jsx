9ea5cb7ce20e668ae2e9689723c09e4989dd081d
Nicolas Meza <nickismeza@gmail.com>
2022-08-03 14:48:29 -0700

    avatar section

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -1,14 +1,28 @@
-import React,{useState,useEffect, useRef} from 'react'
-import axios from 'axios'
+import React,{useState} from 'react'
 export default function Account(props) {
-  const [name1,setName1]=useState('')
-  const [name2,setName2]=useState('')
+  const[username,setUsername]=useState('NicolasM');
+  const[input1,setInput1]=useState('');
+  
+  const handleClick=(e)=>{
+     setUsername('@'+input1);
+    console.log(username)
+  }
+
+  const name1=(name)=>{
+
+    setInput1(name.target.value)
+  }
+
   return (
     <div>
       <h2>Create your Avatar</h2>
-      <input value={name1} type="text" />
-      <input value={name2} type="text" />
-      <button onChange={()=>setName()}>New Account</button>
+      <form >
+      <h2>{input1}</h2>
+      <input onChange={name1} value={input1} type="text" />
+      <button type="button" onClick={handleClick} >New Account</button>
+      </form>
+      <h3>{username}</h3>
+      <img src={`https://avatars.dicebear.com/api/big-smile/${username}.svg`} alt="" />
     </div>
   )
 }
