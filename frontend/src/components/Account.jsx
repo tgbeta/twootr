cf722c667ed51a7c77f389e42f052c1f6984eb06
Nicolas Meza <nickismeza@gmail.com>
2022-08-04 14:13:47 -0700

    styling

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -2,41 +2,36 @@ import React,{useState} from 'react';
 import styled from 'styled-components';
 
 const StyledUserSection=styled.section`
+background:url('https://img.freepik.com/premium-photo/panorama-white-marble-texture-background-tiles-floor-decorative-design_42682-513.jpg?w=2000');
 display:flex;
 flex-direction:column;
 justify-content:center;
 align-items:center;
 `
-const StyledForm=styled.form`
-  
-`
-export default function Account(props) {
-  const[username,setUsername]=useState('@NicolasM');
-  const[input1,setInput1]=useState('');
+export default function Account() {
+  const[username,setUsername]=useState('@Nicolas-Mateo');
+  const[input1,setInput1]=useState('Nicolas Mateo');
   
   const handleClick=(e)=>{
-     setUsername('@'+input1);
-    console.log(username)
+     setUsername('@'+input1.replace(/ /,"-"));
   }
 
   const name1=(name)=>{
-
-    setInput1(name.target.value)
+    const username=name.target.value
+    setInput1(username)
   }
 
   return (
 
     <StyledUserSection>
     <div>
-
-      <h2>Create your Avatar</h2>
+    <img style={{height:"auto",width:"270px",marginTop:"30px"}} className='avatar' src={`https://avatars.dicebear.com/api/big-smile/${username}.svg`} alt="" />
       <form >
-      <h2>{input1}</h2>
+      <div style={{height:"40px",marginTop:"10px"}}><h2 style={{textAlign:"center",fontSize:"35px"}}>{input1}</h2></div>
+      </form>
+      <div style={{display:"flex",justifyContent:'center',alignItems:"center",marginTop:"10px",marginBottom:"20px"}}><h3 style={{color:"#f57171"}}>{username}</h3></div>
       <input onChange={name1} value={input1} type="text" />
       <button type="button" onClick={handleClick} >New Account</button>
-      </form>
-      <img style={{height:"auto",width:"270px"}} className='avatar' src={`https://avatars.dicebear.com/api/big-smile/${username}.svg`} alt="" />
-      <h3>{username}</h3>
     </div>
     </StyledUserSection>
     
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import { StyledHeader } from '../styles/Header.styles';
-
+import styled from 'styled-components';
 
 export default function Header() {
+  const StyledHeader = styled.header`
+  display:flex;
+  align-items:center;
+  justify-content:space-between;
+  background-color:#414141;
+  height:80px;
 
+  .logo{
+    border-radius:20px;
+    height:fit-content;
+    padding-left:10px;
+    padding-right:10px;
+    color:white;
+    font-family:Monoton;
+    font-size:45px;
+    letter-spacing:3px;
+  }
+  .new-tweet{
+    border-radius:15px;
+    height:fit-content;
+    padding:7px;
+    margin-right:60px;
+  }
+  .new-tweet a{
+    color:white;
+    font-size:28px;
+  }
+`;
   return (
     
     <StyledHeader>
-        <div style={{paddingLeft:"40px"}}>
-          <p id="logo">TWOOTR</p>
+        <div style={{marginLeft:"60px"}}>
+          <a href='' className='logo'id='log'>TWOOTR</a>
         </div>
-        <div>
-          <button>Write a new tweet</button>
+        <div className='new-tweet'id="bnt">
+          <a href=''>Write a new tweet</a>
         </div>
     </StyledHeader>
   )
